a365d94e2e3f5b1c068c6efb2fdd2e5b2da75dc1
nedhmn <hermannned@gmail.com>
2025-05-10 13:23:40 -0400

    feat: nextra docs

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,10 +1,25 @@
-import type { NextConfig } from "next";
+import nextra from "nextra";
 
-const nextConfig: NextConfig = {
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+});
+
+// Export the final Next.js config with Nextra included
+export default withNextra({
   // Build static files to deploy to Github Pages
   // ref: https://nextjs.org/docs/app/getting-started/deploying#static-export
   output: "export",
   basePath: "/ygo-ruling-ai-chatbot",
-};
-
-export default nextConfig;
+  // Need to set unoptimized to true for nextra static export
+  // ref: https://nextra.site/docs/guide/static-exports#configuration
+  images: {
+    unoptimized: true,
+  },
+  turbopack: {
+    // ref: https://nextra.site/docs/file-conventions/mdx-components-file#module-not-found-cant-resolve-next-mdx-import-source-file
+    resolveAlias: {
+      "next-mdx-import-source-file": "./mdx-components.tsx",
+    },
+  },
+});

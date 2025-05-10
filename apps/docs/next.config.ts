7da89b8f8afa4f2b9dfc8c00761854c80216b5b1
nedhmn <hermannned@gmail.com>
2025-05-10 11:40:28 -0400

    feat: turborepo ci cache

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Build static files to deploy to Github Pages
+  // ref: https://nextjs.org/docs/app/getting-started/deploying#static-export
   output: "export",
   basePath: "/ygo-ruling-ai-chatbot",
 };

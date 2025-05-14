30dfaa0dae821c838037dc73f5092da22512965f
nedhmn <hermannned@gmail.com>
2025-05-13 20:49:49 -0400

    feat: get-pinecone-index

--- packages/pinecone/src/config.ts
+++ packages/pinecone/src/config.ts
@@ -15,6 +15,9 @@ const envSchema = z.object({
     .describe(
       "Vector database dimensions need to match embedding model dimensions"
     ),
+  PINECONE_INDEX_METRIC: z
+    .enum(["cosine", "dotproduct", "euclidean"] as const)
+    .default("cosine"),
   PINECONE_INDEX_CLOUD: z.enum(["aws", "azure", "gcp"] as const).default("aws"),
   PINECONE_INDEX_REGION: z.string().default("us-east-1"),
 });
--- packages/pinecone/src/utils.ts
+++ packages/pinecone/src/utils.ts
@@ -1,5 +1,30 @@
 import pc from "#client.js";
+import env from "#config.js";
 
-async function getPineconeIndex() {}
+async function getPineconeIndex() {
+  const indexes = (await pc.listIndexes()).indexes;
+
+  // Create the Index if it doesn't exist
+  if (
+    !indexes ||
+    indexes.filter((i) => i.name === env.PINECONE_INDEX_NAME).length !== 1
+  ) {
+    await pc.createIndex({
+      name: env.PINECONE_INDEX_NAME,
+      dimension: env.PINECONE_INDEX_DIMENSION,
+      metric: env.PINECONE_INDEX_METRIC,
+      spec: {
+        serverless: {
+          cloud: env.PINECONE_INDEX_CLOUD,
+          region: env.PINECONE_INDEX_REGION,
+        },
+      },
+      waitUntilReady: true,
+      suppressConflicts: true,
+    });
+  }
+
+  return pc.index(env.PINECONE_INDEX_NAME);
+}
 
 export async function findRelevantContext() {}
